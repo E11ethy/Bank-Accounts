b6d3ed86dda7d30ff9647f7b6ae5ea64fb570c58
Abdallah Ellethy <72635539+E11ethy@users.noreply.github.com>
2024-02-14 16:36:37 +0200

    Update Account.java

--- bank-accounts/src/main/java/com/abcbank/bankaccounts/Account.java
+++ bank-accounts/src/main/java/com/abcbank/bankaccounts/Account.java
@@ -10,23 +10,30 @@ import jakarta.persistence.Id;
 @Entity
 public class Account {
 	
-	    @Id
-	    @GeneratedValue(strategy = GenerationType.IDENTITY)
-	    private Long id;
-	    private String accountHolderName;
-	    private double balance;
-	    
-		public double getBalance() {
-			return balance;
-		}
-		public void setBalance(double balance) {
-			this.balance = balance;
-		}
-		public String getAccountHolderName() {
-			return accountHolderName;
-		}
-		public void setAccountHolderName(String accountHolderName) {
-			this.accountHolderName = accountHolderName;
-		}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String accountHolderName;
+    private double balance;
+
+	public Account() {};
+	public Account(String accountHolderName, double balance) {
+		super();
+		this.accountHolderName = accountHolderName;
+		this.balance = balance;
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	public String getAccountHolderName() {
+		return accountHolderName;
+	}
+	public void setAccountHolderName(String accountHolderName) {
+		this.accountHolderName = accountHolderName;
+	}
 
 }

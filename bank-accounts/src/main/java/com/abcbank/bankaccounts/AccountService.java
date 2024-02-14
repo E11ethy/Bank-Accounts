84b5eabe5d3b46176e0a6c0549847bf9fd5d8cf6
Abdallah Ellethy <72635539+E11ethy@users.noreply.github.com>
2024-02-14 16:40:55 +0200

    Update AccountService.java

--- bank-accounts/src/main/java/com/abcbank/bankaccounts/AccountService.java
+++ bank-accounts/src/main/java/com/abcbank/bankaccounts/AccountService.java
@@ -1,16 +1,21 @@
 package com.abcbank.bankaccounts;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 
 @Service
 public class AccountService {
 	
-	@Autowired
+	
     private AccountRepository accountRepository;
+    
+    public AccountService(AccountRepository accountRepository) {
+		super();
+		this.accountRepository = accountRepository;
+	}
 
     public Account createAccount(Account account) {
         return accountRepository.save(account);
@@ -19,8 +24,15 @@ public class AccountService {
     public Optional<Account> getAccount(Long id) {
         return accountRepository.findById(id);
     }
+    
+    public List<Account> all() {
+        return accountRepository.findAll();
+    }
 
-    public Account deposit(Long id, double amount) {
+
+
+
+	public Account deposit(Long id, double amount) {
         Account account = getAccount(id).orElseThrow(() -> new RuntimeException("Account not found"));
         account.setBalance(account.getBalance() + amount);
         return accountRepository.save(account);

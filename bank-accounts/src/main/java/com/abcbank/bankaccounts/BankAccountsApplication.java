27540345e8f8d21f4c59469d2bdd89bf004922e1
Abdallah Ellethy <72635539+E11ethy@users.noreply.github.com>
2024-02-14 16:31:42 +0200

    Update BankAccountsApplication.java

--- bank-accounts/src/main/java/com/abcbank/bankaccounts/BankAccountsApplication.java
+++ bank-accounts/src/main/java/com/abcbank/bankaccounts/BankAccountsApplication.java
@@ -6,8 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BankAccountsApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(BankAccountsApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BankAccountsApplication.class, args);
+	}
+
+	CommandLineRunner initDatabase (AccountRepository repository) {
+		return arge -> {
+			log.info("Preloading " + repository.save(new Account ("Abdallah", 100)));
+			log.info("preloading " + repository.save(new Accoun ("Ellethy", 200)));
+		}
 	}
 
 }
